931f8d2395d576c90148dec9a2c79310f386f5b5
4ever2 <3417013+4ever2@users.noreply.github.com>
2024-10-29 16:00:17 +0100

    Test MetaCoq translations

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/355e12b665339290efd7d08180d423973d70228f.tar.gz;
-    sha256 = "0rw76fniaz2ihw2nhn2fhn2b6mcl2y6r49dv4hjb9n4w9q9csak5";
-  }
+         url = https://github.com/4ever2/nixpkgs/archive/c2649ebb451e33aedc7141d1ced3321244dc2500.tar.gz;
+         sha256 = "19hqi4nbb44417ps22cgp1rpsapg9k7zadzknppyrknlbq35b1sd";
+       }
